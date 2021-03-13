cc6470524778af1beb8b818fbbc9c9444b0489e3
AntonioK180 <antonio.kasabov@gmail.com>
2021-03-13 14:16:12 +0200

    Made the first 3 html pages (structure).

--- app.py
+++ app.py
@@ -9,11 +9,19 @@ mysql = MySQL(app)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_PORT'] = 3308
-app.config['MYSQL_USER'] = ''
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Test_123'
 
 
 @app.route('/')
 def home():
     cur = mysql.connection.cursor()
     return render_template('home.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')

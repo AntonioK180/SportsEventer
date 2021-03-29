9d6d68190ec8873194464bdbe403a54ec872c136
AntonioK180 <antonio.kasabov@gmail.com>
2021-03-29 10:16:57 +0300

    Added an UPDATE operation.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import logging
 from event import Event
 
@@ -31,7 +31,34 @@ def newEvent():
 
         new_event = Event(None, sport, people_participating, people_needed, date_time, location, price, description)
         new_event.create()
+
     return render_template('newEvent.html')
+
+
+@app.route('/events/<int:event_id>')
+def openEvent(event_id):
+    event = Event.find(event_id)
+
+    return render_template('event.html', event = event)
+
+
+@app.route('/events/<int:event_id>/edit', methods=['GET', 'POST'])
+def edit_event(event_id):
+    event = Event.find(event_id)
+    if request.method == 'GET':
+
+        return render_template('edit_event.html' , event = event)
+    elif request.method == 'POST':
+        event.sport = request.form['sport']
+        event.people_participating = request.form['people_participating']
+        event.people_needed = request.form['people_needed']
+        event.date_time = request.form['date_time']
+        event.location = request.form['location']
+        event.price = request.form['price']
+        event.description = request.form['description']
+        event.save()
+
+        return redirect(url_for('openEvent', event_id = event.event_id))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- event.py
+++ event.py
@@ -19,9 +19,34 @@ class Event():
         result = cur.fetchall()
         return [Event(*row) for row in result]
 
+    @staticmethod
+    def find(event_id):
+        cur = mydb.cursor()
+        query = "SELECT * FROM Events WHERE id = %s"
+        value = (event_id,)
+        cur.execute(query, value)
+        row = cur.fetchone()
+        if row is None:
+            return
+        return Event(*row)
+
     def create(self):
         cur = mydb.cursor()
         query = "INSERT INTO Events (sport, people_participating, people_needed, date_time, location, price, description) VALUES (%s, %s, %s, %s, %s, %s, %s)"
         value = (self.sport, self.people_participating, self.people_needed, self.date_time, self.location, self.price, self.description)
+        cur.execute(query, value)
+        mydb.commit()
+
+    def save(self):
+        cur = mydb.cursor()
+        query = "UPDATE Events SET people_participating = %s, people_needed = %s, date_time = %s, location = %s, price = %s, description = %s WHERE id = %s"
+        value = (self.people_participating, self.people_needed, self.date_time, self.location, self.price, self.description, self.event_id)
+        cur.execute(query, value)
+        mydb.commit()
+
+    def delete(self):
+        cur = mydb.cursor()
+        query = "DELETE FROM Events WHERE event_id = %s"
+        value = (self.event_id)
         cur.execute(query, value)
         mydb.commit()

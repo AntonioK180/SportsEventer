3251eeae2d8996c64c82e4c720811f441859ef63
AntonioK180 <antonio.kasabov@gmail.com>
2021-10-12 19:23:57 +0300

    Opened in VSCode and the HTL and JS Formatters just did wha they wanted to. I think this comment gets too long.

--- chat_poc/login.js
+++ chat_poc/login.js
@@ -1,12 +1,12 @@
 // FirebaseUI config.
 var uiConfig = {
-  signInSuccessUrl: "index.html",
-  signInOptions: [
-    // Leave the lines as is for the providers you want to offer your users.
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
-  ],
-  // Terms of service url.
-  tosUrl: "index.html",
+    signInSuccessUrl: "index.html",
+    signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    ],
+    // Terms of service url.
+    tosUrl: "index.html",
 };
 
 // Initialize the FirebaseUI Widget using Firebase.
